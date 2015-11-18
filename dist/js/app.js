0c48cf79b05725604ee7af2fc27c1392c6845a78
ayezee33 <you@example.com>
2015-11-18 10:39:30 -0800

    c2 fixed js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,7 @@
+function formIO() {
+    var text = document.getElementById("name").value;
+
+    document.getElementById("output").innerHTML = text;
+}
 
 //# sourceMappingURL=app.js.map
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+function formIO() {
+    var text = document.getElementById("name").value;
+
+    document.getElementById("output").innerHTML = text;
+}
